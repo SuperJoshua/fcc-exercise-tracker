2b60e7311599b3531a73650d34239fbedf7ebd35
Joshua Wright <1981283+SuperJoshua@users.noreply.github.com>
2025-05-08 22:10:39 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express')
+"use strict"
+
+const body_parser = require("body-parser")
+const cors = require("cors")
+const express = require("express")
+const fs = require("node:fs")
+
+
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
-
+app.use(body_parser.urlencoded({extended: false}))
 app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.use(express.static("./public"))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "./views/index.html")
+})
 
 
 
 
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+const app_port = 3000
+app.listen(app_port, () => {
+  console.log(`Your app is listening on port ${app_port}`)
 })
